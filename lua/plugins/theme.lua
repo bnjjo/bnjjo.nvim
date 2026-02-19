0d5f4a449eca1fe83d02905f1c9813fa5e85d09f
benjo <bsukalo@outlook.com>
2026-02-19 23:29:36 +0100

    tokyonight babyyy

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,44 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
+	name = "tokynight",
 	config = function()
-		vim.cmd.colorscheme("catppuccin")
+		require("tokyonight").setup({
+			transparent = true,
+
+			-- borderless telescope
+			on_highlights = function(hl, c)
+				local prompt = "#2d3149"
+				hl.TelescopeNormal = {
+					bg = c.bg_dark,
+					fg = c.fg_dark,
+				}
+				hl.TelescopeBorder = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopePromptNormal = {
+					bg = prompt,
+				}
+				hl.TelescopePromptBorder = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePreviewTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopeResultsTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+			end,
+		})
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
