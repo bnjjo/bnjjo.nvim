bebcf538c965591bb6be27669381513f7272cde5
bnjjo <131111426+bnjjo@users.noreply.github.com>
2026-06-18 22:33:28 +0200

    add org mode channel

--- plugin/tv.lua
+++ plugin/tv.lua
@@ -39,5 +39,16 @@ require("tv").setup({
 				["<C-y>"] = h.copy_to_clipboard, -- copy paths to clipboard
 			},
 		},
+		-- `org`: fuzzy find org-mode files
+		org = {
+			keybinding = "<leader>so",
+			handlers = {
+				["<CR>"] = h.open_as_files, -- default: open selected files
+				["<C-q>"] = h.send_to_quickfix, -- send to quickfix list
+				["<C-s>"] = h.open_in_split, -- open in horizontal split
+				["<C-v>"] = h.open_in_vsplit, -- open in vertical split
+				["<C-y>"] = h.copy_to_clipboard, -- copy paths to clipboard
+			},
+		},
 	},
 })
